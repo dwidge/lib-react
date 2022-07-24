5869747eec32922e89d52f636be8d331bdd5ce0e
dwidge <dwidge+gh@gmail.com>
2022-07-24 18:46:48 +0200

    fix dependency

--- storage.js
+++ storage.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react'
-
 function getStorageValue(key, defaultValue) {
 	const saved = window && localStorage.getItem(key)
 	const initial = JSON.parse(saved)
 	return initial || defaultValue
 }
 
-export const useStorage = (key, defaultValue) => {
-	const [value, setValue] = useState(() =>
-		getStorageValue(key, defaultValue),
-	)
+export default (useState, useEffect) => ({
+	useStorage(key, defaultValue) {
+		const [value, setValue] = useState(() =>
+			getStorageValue(key, defaultValue),
+		)
 
-	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(value))
-	}, [key, value])
+		useEffect(() => {
+			localStorage.setItem(key, JSON.stringify(value))
+		}, [key, value])
 
-	return [value, setValue]
-}
+		return [value, setValue]
+	},
+})
